0c7af4c724012cb81448a9c74b347f8f250eee94
itay-oren <itayor053@gmail.com>
2022-02-20 01:27:32 +0200

    wow

--- exe1p2/exe1p2.cpp
+++ exe1p2/exe1p2.cpp
@@ -11,16 +11,22 @@ using namespace std;
 int main() {
     int input;
     char name[21];
-    bool flag = true;
+    bool flag = true, first = true;
     //asking from user to enter details:
     Worker lowestWork, highestWork, tempWorker;
     cout << "enter details, to end enter 0:" << endl;
 
-    while (input != 0)//untill 0 is entered do:
+    /*
+     * TO DO:
+     * make shure that the first worker enter to both high and low in the first iteration
+     *
+     */
+
+    while (input != 0)//until 0 is entered do:
     {
         cin >> input;
         if (input == 0)// if enter 0 is entered finnish the loop
-            continue;
+            break;
         if (input < 0)
             flag = false;
         tempWorker.setID(input);//if something else is entered it is the Worker id
@@ -45,19 +51,22 @@ int main() {
         tempWorker.setIncome(input);
 
         if (flag) {
-            if (tempWorker.getIncome() < lowestWork.getIncome()) {
+            if (tempWorker.getIncome() < lowestWork.getIncome() || first) {
                 lowestWork.setID(tempWorker.getID());
                 lowestWork.setName(tempWorker.getName());
                 lowestWork.setIncome(tempWorker.getIncome());
             }
-            if (tempWorker.salaryCalc() > highestWork.salaryCalc()) {
+            if (tempWorker.salaryCalc() > highestWork.salaryCalc() || first) {
                 highestWork.setID(tempWorker.getID());
                 highestWork.setName(tempWorker.getName());
                 highestWork.setIncome(tempWorker.getIncome());
                 highestWork.setHours(tempWorker.getHours());
                 highestWork.setSalary(tempWorker.getIncome());
             }
+            first = false;
         }
+        else
+            cout << "ERROR";
 
     }
     cout << "minimum collected: " << lowestWork.getID() << " " << lowestWork.getName() << " " << lowestWork.getIncome()
