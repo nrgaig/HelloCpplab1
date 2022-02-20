9b076869c8b4a8c4ff4c6c3ce67223d4a3201ed5
itay-oren <65827754+itay-oren@users.noreply.github.com>
2022-02-20 22:55:44 +0200

    work on clion

--- exe1p2/exe1p2.cpp
+++ exe1p2/exe1p2.cpp
@@ -9,21 +9,16 @@
 using namespace std;
 
 int main() {
-    int input;
+    float input;
     char name[21];
     bool flag = true, first = true;
     //asking from user to enter details:
     Worker lowestWork, highestWork, tempWorker;
     cout << "enter details, to end enter 0:" << endl;
 
-    /*
-     * TO DO:
-     * make shure that the first worker enter to both high and low in the first iteration
-     *
-     */
-
     while (input != 0)//until 0 is entered do:
     {
+        flag = true;
         cin >> input;
         if (input == 0)// if enter 0 is entered finnish the loop
             break;
@@ -31,9 +26,13 @@ int main() {
             flag = false;
         tempWorker.setID(input);//if something else is entered it is the Worker id
 
-        cin.get();//avoiding empty string
-        cin.getline(name, 20);//inputting string from user
+        cin >> name;
         tempWorker.setName(name);
+
+        cin >> input;
+        if (input < 0)
+            flag = false;
+        tempWorker.setSalary(input);
 
         cin >> input;//input hours
         if (input < 0)
@@ -43,14 +42,11 @@ int main() {
         cin >> input;
         if (input < 0)
             flag = false;
-        tempWorker.setSalary(input);
-
-        cin >> input;
-        if (input < 0)
-            flag = false;
         tempWorker.setIncome(input);
 
-        if (flag) {
+        if (!flag) {
+            cout << "ERROR" << endl;
+        }
             if (tempWorker.getIncome() < lowestWork.getIncome() || first) {
                 lowestWork.setID(tempWorker.getID());
                 lowestWork.setName(tempWorker.getName());
@@ -61,14 +57,12 @@ int main() {
                 highestWork.setName(tempWorker.getName());
                 highestWork.setIncome(tempWorker.getIncome());
                 highestWork.setHours(tempWorker.getHours());
-                highestWork.setSalary(tempWorker.getIncome());
+                highestWork.setSalary(tempWorker.getSalary());
+                first = false;
             }
             first = false;
-        }
-        else
-            cout << "ERROR";
-
     }
     cout << "minimum collected: " << lowestWork.getID() << " " << lowestWork.getName() << " " << lowestWork.getIncome()
-         << endl << "highest salary: " << highestWork.getID() << " " << highestWork.getName() << " " << highestWork.salaryCalc();
+         << endl << "highest salary: " << highestWork.getID() << " " << highestWork.getName() << " "
+         << highestWork.salaryCalc();
 }
