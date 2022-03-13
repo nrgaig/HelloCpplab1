a3d2c4f32dff2a5952f377aa85ad65227eac44e4
MrFrost <89649789+nrgaig@users.noreply.github.com>
2022-03-13 22:06:37 +0200

    add exe3 p1 head comments (and new branch test)

--- exe3/p1/Rational.h
+++ exe3/p1/Rational.h
@@ -2,6 +2,7 @@
   Created by Maor Frost 206370231 and Itay Oren 318648482
   cpp lab - Meir Litman
   exercise 3 task 1
+  class header represent rational number
 ****************************************************/
 
 #ifndef HELLOCPPLAB1_RATIONAL_H
