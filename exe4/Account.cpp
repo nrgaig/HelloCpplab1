1975fd189c30feee75eaa1fb8f4bb0eae2e4e677
itay-oren <65827754+itay-oren@users.noreply.github.com>
2022-03-21 17:08:38 +0200

    a lot of problems

---
+++ exe4/Account.cpp
@@ -0,0 +1,89 @@
+//
+// Created by itayo on 21/03/2022.
+//
+
+#include "Account.h"
+#include <iostream>
+
+using namespace std;
+
+int Account::sumWithdraw = 0;
+int Account::sumDeposit = 0;
+
+
+Account::Account() : accountNumber(0), code(0), balance(0), email("") {} // empty constructor
+
+Account::Account(int _accountNumber, int _code, int _balance, string _email) {
+    if (_code < 1000)
+        throw "ERROR: code must be of 4 digits!\n";
+    size_t locationOfShtrudel = _email.find('@');
+    if (locationOfShtrudel == string::npos)
+        throw "ERROR: email must contain @!\n";
+    size_t locationOfCom = _email.find(".com", locationOfShtrudel + 1);
+    size_t locationOfComIl = _email.find(".co.il", locationOfShtrudel + 1);
+    if (locationOfComIl == string::npos && locationOfCom == string::npos)
+        throw "ERROR: email must end at .com or .co.il!\n";
+    accountNumber = _accountNumber;
+    code = _code;
+    balance = _balance;
+    email = _email;
+
+}
+
+int Account::getAccountNumber() const {
+    return accountNumber;
+}
+
+int Account::getCode() const {
+    return code;
+}
+
+int Account::getBalance() const {
+    return balance;
+}
+
+const string &Account::getEmail() const {
+    return email;
+}
+
+istream &operator>>(istream &is, Account &ms) {
+    int _accountNumber, _code;
+    string _email;
+    is >> _accountNumber >> _code >> _email;
+    if (_code < 1000)
+        throw "ERROR: code must be of 4 digits!\n";
+    size_t locationOfShtrudel = _email.find('@');
+    if (locationOfShtrudel == string::npos)
+        throw "ERROR: email must contain @!\n";
+    size_t locationOfCom = _email.find(".com", locationOfShtrudel + 1);
+    size_t locationOfComIl = _email.find(".com,il", locationOfShtrudel + 1);
+    if (locationOfComIl == string::npos && locationOfCom == string::npos)
+        throw "ERROR: email must end at .com or .co.il!\n";
+    ms.accountNumber = _accountNumber;
+    ms.code = _code;
+    ms.email = _email;
+    return is;
+}
+
+
+void Account::withdraw(int nis) {
+    if (balance - nis < -6000)
+        throw "ERROR: cannot have less than - 6000 NIS!\n";
+    if (nis > 2500)
+        throw "ERROR: cannot withdraw more than 2500 NIS!\n";
+    balance -= nis;
+}
+
+void Account::deposit(int nis) {
+    if (nis > 10000)
+        throw "ERROR: cannot deposit more than 10000 NIS!\n";
+    balance += nis;
+}
+
+int Account::getSumWithdraw() {
+    return sumWithdraw;
+}
+
+int Account::getSumDeposit() {
+    return sumDeposit;
+}
--- exe4/Clock.h
+++ exe4/Clock.h
@@ -4,11 +4,37 @@
 
 #ifndef HELLOCPPLAB1_CLOCK_H
 #define HELLOCPPLAB1_CLOCK_H
+#include <iostream>
+using namespace std;
 
 
 class Clock {
+public:
+    Clock(int _hour = 0, int _minute = 0, int _second = 0); // constructor
+
+    int getHour() const;
+
+    int getMinute() const;
+
+    int getSecond() const;
+
+    void setHour(int _hour);
+
+    void setMinute(int _minute);
+
+    void setSecond(int _second);
+
+    Clock operator+=(int _minuteToAdd);
 
 
+    friend ostream& operator<<(ostream& os, const Clock& ms); // ostream operator
+    friend istream& operator>>(istream& is, Clock& ms);// istream operator
+
+
+private:
+    int hour;
+    int minute;
+    int second;
 };
 
 
