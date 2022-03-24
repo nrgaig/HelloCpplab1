b3e0ce5865613a80d1c61b483b77e3c161e9c0eb
itay-oren <itayor053@gmail.com>
2022-03-24 23:12:37 +0200

    add ex5

---
+++ exe5/p1/List.h
@@ -0,0 +1,17 @@
+/***********************************************************
+ * Created by Maor Frost 206370231 and Itay Oren 318648482 *
+ * cpp lab - Meir Litman                                   *
+ * exercise 5 task 1                                       *
+ *   *
+************************************************************/
+
+#ifndef HELLOCPPLAB1_LIST_H
+#define HELLOCPPLAB1_LIST_H
+
+
+class List {
+
+};
+
+
+#endif //HELLOCPPLAB1_LIST_H
---
+++ exe5/p1/exe5p1.cpp
@@ -0,0 +1,7 @@
+/***********************************************************
+ * Created by Maor Frost 206370231 and Itay Oren 318648482 *
+ * cpp lab - Meir Litman                                   *
+ * exercise 5 task 1                                       *
+ * main program we requested to use from exercise author   *
+************************************************************/
+
