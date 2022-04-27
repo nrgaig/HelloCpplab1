1e95d4a8583e8ff332dd9b838a0472789d1c81ce
itayoren <itayor053@gmail.com>
2022-04-27 20:48:46 +0300

    56

--- exe6/p1/PartTime.h
+++ exe6/p1/PartTime.h
@@ -2,11 +2,13 @@
 // Created by itayo on 25/04/2022.
 //
 
+#include "Employee.h"
+
 #ifndef HELLOCPPLAB1_PARTTIME_H
 #define HELLOCPPLAB1_PARTTIME_H
 
 
-class PartTime {
+class PartTime : public Employee {
 
 };
 
