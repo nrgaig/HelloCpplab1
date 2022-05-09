84c48dcd70c0b541224123dfb6069f0226d001e7
itay-oren <65827754+itay-oren@users.noreply.github.com>
2022-05-09 23:02:18 +0300

    הj

--- exe7/p1/Circle.cpp
+++ exe7/p1/Circle.cpp
@@ -1,5 +1,27 @@
-//
-// Created by itayo on 08/05/2022.
-//
+/***********************************************************
+ * Created by Maor Frost 206370231 and Itay Oren 318648482 *
+ * cpp lab - Meir Litman                                   *
+ * exercise 7 task 1                                       *
+ *                          *
+************************************************************/
 
-#include "Circle.h"
+#ifndef HELLOCPPLAB1_SHAPE_H
+#define HELLOCPPLAB1_SHAPE_H
+
+#include "Shape.cpp"
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+class Circle : public Shape {
+public:
+
+
+protected:
+    float radius;
+    Point center;
+
+};
+
+
+#endif //HELLOCPPLAB1_SHAPE_H
