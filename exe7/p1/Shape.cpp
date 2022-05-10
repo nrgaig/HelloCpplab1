02f673bebd423a9608822196f938a7e21894588f
MrFrost <89649789+nrgaig@users.noreply.github.com>
2022-05-10 10:40:01 +0300

    exe7 2

--- exe7/p1/Shape.cpp
+++ exe7/p1/Shape.cpp
@@ -2,7 +2,7 @@
  * Created by Maor Frost 206370231 and Itay Oren 318648482 *
  * cpp lab - Meir Litman                                   *
  * exercise 7 task 1                                       *
- * Abstarct class represent Shape                          *
+ * Abstract class represent Shape                          *
 ************************************************************/
 
 #ifndef HELLOCPPLAB1_SHAPE_H
@@ -13,6 +13,7 @@
 using namespace std;
 
 class Shape {
+private:
     class Point {
     public:
         Point():x(0), y(0) {}//empty constructor
@@ -21,26 +22,25 @@ class Shape {
 
         Point(const Point &copy):x(copy.x), y(copy.y) {} // copy constructor
 
-
-
-        float distance(Point point) {
+        double distance(const Point& point) const {
             return sqrt(pow(point.x- this->x,2)+pow(point.y- this->y,2));
         }
 
-        int getX()  {return x;}
+        int getX() const  {return x;}
 
-        int getY()  {return y;}
+        int getY() const  {return y;}
 
-        void setX(int x) {Point::x = x;}
+        void setX(int newX) { this->x = newX;}
 
-        void setY(int y) {y = y;}
+        void setY(int newY) { this->y = newY;}
 
         friend istream & operator>>(istream &is, Point &ms) { //overloading >> operator
-            int x,y;
+            int newX,newY;
             char trash;
-            is >> trash >> x >> trash >> y >> trash;
-            ms.setX(x);
-            ms.setY(y);
+            is >> trash >> newX >> trash >> newY >> trash;//input format: "(newX,newY)"
+            ms.setX(newX);
+            ms.setY(newY);
+            return is;
         }
 
     private:
@@ -48,9 +48,50 @@ class Shape {
         int y;
 
 
-    };
+    };//end of class Point
 
-protected:
+protected: //properties
+    int numOfPoints;
+    Point * points;
+
+public:
+    explicit Shape(int numOfPoints) : numOfPoints(numOfPoints), points(new Point[numOfPoints]) {// ctor
+        cout<<"Enter values of  "<< numOfPoints <<" points: \n"; //asking input from user
+        for (int i = 0; i <numOfPoints; ++i) {//running on array of points
+            cin>>
+               points[i]; //getting values from user
+        }
+    }//end ctor
+
+
+    Shape() : numOfPoints(0), points(nullptr){} // empty ctor
+
+    Shape(const Shape &copy) : numOfPoints(copy.numOfPoints),points(new Point[numOfPoints]){ // deep copy ctor
+        for (int i = 0; i <numOfPoints; ++i) {//running on array of points
+            points[i].setX(copy.points[i].getX()); //copy x values
+            points[i].setY(copy.points[i].getY()); //copy y values
+        }
+    }// end of copy ctor
+
+    Shape(const Shape &&copy)  noexcept : numOfPoints(copy.numOfPoints) ,points(copy.points){}// move ctor
+
+    virtual ~Shape() {delete [] points;} // dtor
+
+    friend ostream & operator<<(ostream &os, Shape & shape){ // output for all points values in specific format
+        // format: "points: (x1,y1) (x2, y2)... (xn,yn)"
+        os << "points:";
+        for (int i = 0; i < shape.numOfPoints; ++i) {
+            os << " (" <<shape.points[i].getX()<<","<<shape.points[i].getY()<<")";
+        }
+        os<<"\n";
+        return os;
+    }//end of operator<<
+
+    virtual float area() const = 0; //return area of shape
+
+    virtual bool isSpecial() const = 0;// return true if shape has special feature
+
+    virtual void printSpecial() const = 0; // printing all special  features
 
 };
 
