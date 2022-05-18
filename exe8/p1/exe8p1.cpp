5e987f78f08173fc6b8d5b20db48b494b9d8992f
itay-oren <65827754+itay-oren@users.noreply.github.com>
2022-05-19 01:06:07 +0300

    add exe8 directory

--- exe8/p1/exe8p1.cpp
+++ exe8/p1/exe8p1.cpp
@@ -21,120 +21,69 @@ string infixToPostfix(string exp) {
         ch = exp[i];
         if (ch == '(') {
             stack.push(ch);
-        }
-        else if (ch == ')') {
+        } else if (ch == ')') {
             str.push_back(32);//adding space between every character
             while (stack.top() != '(') {
                 str += stack.top();
                 stack.pop();
-                str.push_back(32);//adding space between every character
+//                str.push_back(32);//adding space between every character
             }
             stack.pop();
-        }
-        else if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
-            while (!stack.isEmpty() && stack.top() != '(' && (stack.top() == '+' || stack.top() == '-' || stack.top() == '*' || stack.top() == '/')) {
+        } else if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+            while (!stack.isEmpty() && stack.top() != '(' &&
+                   (stack.top() == '+' || stack.top() == '-' || stack.top() == '*' || stack.top() == '/')) {
                 str += stack.top();
                 stack.pop();
+
             }
             stack.push(ch);
             str.push_back(32);//adding space between every character
 
-        }
-        else {
+        } else {
             str += ch;
         }
         i++;
     }
     while (!stack.isEmpty()) {
+        str.push_back(32);//adding space between every character
         str += stack.top();
         stack.pop();
     }
     return str;
-
-
-//    string str; // creating new blank string
-//    StackVector<char> stk; // creating new blank char-stack
-//    char ch;
-//
-//    //ch=exp[0];
-//    int i = 0; //
-//    while (i < exp.length()) {
-//        if (exp[i] == '(') // (
-//            stk.push(exp[i]);
-//
-//        if (exp[i] == ')') { // )
-//            str.push_back(32);//adding space between every character
-//            if (!stk.isEmpty()) {
-//                while (stk.top() != '(') {
-//                    str.push_back(stk.top());
-//                    stk.pop();
-//                    str.push_back(32);//adding space between every character
-//                }
-//                stk.pop();
-//            }
-//
-//        }
-//        if (exp[i] == '+' || exp[i] == '-' || exp[i] == '*' || exp[i] == '/') { // operation
-//            str.push_back(32);//adding space between every character
-//            if (exp[i] == '+' || exp[i] == '-') {
-//                if (!stk.isEmpty()) {
-//                    while (stk.top() != '(' && stk.top() != '+' && stk.top() != '-') {
-//                        str.push_back(stk.top());
-//                        stk.pop();
-//                        str.push_back(32);//adding space between every character
-//                    }
-//                }
-//                stk.push(exp[i]);
-//
-//            } else {
-//                if (!stk.isEmpty()) {
-//                    while (stk.top() != '(') {
-//                        str.push_back(stk.top());
-//                        stk.pop();
-//                        str.push_back(32);//adding space between every character
-//                    }
-//                }
-//                stk.push(exp[i]);
-//            }
-//        }
-//        if (47 < exp[i] && exp[i] < 58) { // digit
-//            str.push_back(exp[i]);
-//
-//        }
-//        i++;
-//    }
-//
-//    return str;
 }
+
 // calculate the result of the postfix expression
 string calcPostfix(string postfix) {
     StackVector<int> stk;
     int i = 0;
-    int num1, num2, result;
+    int spaceIndex;
+    float num1, num2, result;
     while (i < postfix.length()) {
         if (47 < postfix[i] && postfix[i] < 58) { // digit
-            stk.push(stoi(postfix.substr(i, postfix.find(' ', i) - i)));
-            i = (int) postfix.find(' ', i);
-//            stk.push(postfix[i] - '0');
+            spaceIndex = (int) postfix.find(' ', i);
+            stk.push(stoi(postfix.substr(i, spaceIndex - i)));
+            if (spaceIndex > 0) {
+                i = spaceIndex;
+            }
         }
         if (postfix[i] == '+' || postfix[i] == '-' || postfix[i] == '*' || postfix[i] == '/') { // operation
-            num1 = stk.top();
+            num2 = (float) stk.top();
             stk.pop();
-            num2 = stk.top();
+            num1 = (float) stk.top();
             stk.pop();
             if (postfix[i] == '+') {
                 result = num1 + num2;
             }
-            if (postfix[i] == '-') {
+            else if (postfix[i] == '-') {
                 result = num1 - num2;
             }
-            if (postfix[i] == '*') {
+            else if (postfix[i] == '*') {
                 result = num1 * num2;
             }
-            if (postfix[i] == '/') {
+            else if (postfix[i] == '/') {
                 result = num1 / num2;
             }
-            stk.push(result);
+            stk.push((int)result);
         }
         i++;
     }
