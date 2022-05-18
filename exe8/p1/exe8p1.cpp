3028c96c44770b4f2ea960a873ae16653c9f989e
itay-oren <65827754+itay-oren@users.noreply.github.com>
2022-05-19 01:11:27 +0300

    committed

--- exe8/p1/exe8p1.cpp
+++ exe8/p1/exe8p1.cpp
@@ -13,25 +13,23 @@ using namespace std;
 
 //Convert infix expression to postfix expression:
 string infixToPostfix(string exp) {
-    string str;
-    StackVector<char> stack;
-    char ch;
-    int i = 0;
-    while (i < exp.length()) {
+    string str; //postfix expression
+    StackVector<char> stack;    //stack for operators
+    char ch; //current char
+    int i = 0; //current index
+    while (i < exp.length()) { //while there are chars in the expression
         ch = exp[i];
         if (ch == '(') {
             stack.push(ch);
         } else if (ch == ')') {
             str.push_back(32);//adding space between every character
-            while (stack.top() != '(') {
+            while (stack.top() != '(') {    //while top of stack is not '('
                 str += stack.top();
                 stack.pop();
-//                str.push_back(32);//adding space between every character
             }
             stack.pop();
-        } else if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
-            while (!stack.isEmpty() && stack.top() != '(' &&
-                   (stack.top() == '+' || stack.top() == '-' || stack.top() == '*' || stack.top() == '/')) {
+        } else if (ch == '+' || ch == '-' || ch == '*' || ch == '/') { // if operator
+            while (!stack.isEmpty() && stack.top() != '(' && (stack.top() == '+' || stack.top() == '-' || stack.top() == '*' || stack.top() == '/')) { //while top of stack is operator
                 str += stack.top();
                 stack.pop();
 
@@ -39,14 +37,14 @@ string infixToPostfix(string exp) {
             stack.push(ch);
             str.push_back(32);//adding space between every character
 
-        } else {
+        } else { //if char is number
             str += ch;
         }
         i++;
     }
-    while (!stack.isEmpty()) {
+    while (!stack.isEmpty()) { //while stack is not empty
         str.push_back(32);//adding space between every character
-        str += stack.top();
+        str += stack.top(); //add top of stack to postfix expression
         stack.pop();
     }
     return str;
@@ -58,15 +56,14 @@ string calcPostfix(string postfix) {
     int i = 0;
     int spaceIndex;
     float num1, num2, result;
-    while (i < postfix.length()) {
+    while (i < postfix.length()) { //while there are chars in the expression
         if (47 < postfix[i] && postfix[i] < 58) { // digit
-            spaceIndex = (int) postfix.find(' ', i);
-            stk.push(stoi(postfix.substr(i, spaceIndex - i)));
-            if (spaceIndex > 0) {
-                i = spaceIndex;
-            }
+            spaceIndex = (int) postfix.find(' ', i); //find the next space
+            stk.push(stoi(postfix.substr(i, spaceIndex - i))); //push the number to the stack
+                i = spaceIndex; //move to the next char
         }
         if (postfix[i] == '+' || postfix[i] == '-' || postfix[i] == '*' || postfix[i] == '/') { // operation
+            /* get the top 2 numbers on the stack in order to calc them */
             num2 = (float) stk.top();
             stk.pop();
             num1 = (float) stk.top();
