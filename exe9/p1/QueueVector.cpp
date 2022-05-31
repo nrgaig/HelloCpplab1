0ff4efbcb20da1f322411448b00e054677f577c3
itay-oren <65827754+itay-oren@users.noreply.github.com>
2022-05-31 22:49:06 +0300

    Changes by maor

--- exe9/p1/QueueVector.cpp
+++ exe9/p1/QueueVector.cpp
@@ -4,38 +4,3 @@
 
 #include "QueueVector.h"
 
-QueueVector::QueueVector(int size) {
-    capacity = size + 1;
-    data = new int[capacity];
-    clear();
-}
-
-void QueueVector::clear() {
-    nextSlot = 0;
-    firstUse = 0;
-}
-
-int QueueVector::dequeue() {
-    if (isEmpty()) throw "Queue is empty\n";
-    int dataloc = firstUse;
-    ++firstUse %= capacity;
-    return data[dataloc];
-}
-
-void QueueVector::enqueue(int val) {
-    if ((nextSlot + 1) % capacity == firstUse)
-        throw "the Queue is full\n";
-    data[nextSlot] = val;
-    ++nextSlot %= capacity;
-}
-
-int QueueVector::front() {
-    if (isEmpty())
-        throw "Queue is empty\n";
-    return data[firstUse];
-}
-
-bool QueueVector::isEmpty() const {
-    return nextSlot == firstUse;
-}
-
--- exe9/p1/QueueVector.h
+++ exe9/p1/QueueVector.h
@@ -7,8 +7,8 @@
 
 
 #include "Queue.h"
-
-class QueueVector : public Queue {
+template<class T>
+class QueueVector<T> : public Queue {
 public:
     QueueVector(int max);
 
@@ -29,6 +29,42 @@ private:
     int nextSlot;
     int firstUse;
 };
+
+
+QueueVector::QueueVector(int size) {
+    capacity = size + 1;
+    data = new int[capacity];
+    clear();
+}
+
+void QueueVector::clear() {
+    nextSlot = 0;
+    firstUse = 0;
+}
+
+int QueueVector::dequeue() {
+    if (isEmpty()) throw "Queue is empty\n";
+    int dataloc = firstUse;
+    ++firstUse %= capacity;
+    return data[dataloc];
+}
+
+void QueueVector::enqueue(int val) {
+    if ((nextSlot + 1) % capacity == firstUse)
+        throw "the Queue is full\n";
+    data[nextSlot] = val;
+    ++nextSlot %= capacity;
+}
+
+int QueueVector::front() {
+    if (isEmpty())
+        throw "Queue is empty\n";
+    return data[firstUse];
+}
+
+bool QueueVector::isEmpty() const {
+    return nextSlot == firstUse;
+}
 
 
 #endif //HELLOCPPLAB1_QUEUEVECTOR_H
